5b54079998ab8798369e02f1e407475fff3152f0
root <root@nur1labs.nur1labs.jp>
2018-05-18 10:21:26 +0700

    upload

---
+++ website/config.js
@@ -0,0 +1,17 @@
+var api = "https://pool.catalyst.cash";
+
+var poolHost = "pool.catalyst.cash";
+
+var irc = "irc.freenode.net/#cata";
+
+var email = "support@cata.com";
+
+var cryptonatorWidget = ["CATA-BTC", "CATA-USD", "CATA-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "https://explorer.evo.com/block/{id}";
+
+var transactionExplorer = "https://explorer.evo.com/tx/{id}";
+
+var themeCss = "themes/default-theme.css";
